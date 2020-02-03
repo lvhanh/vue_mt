b0e47a1c370897049b4ad16a673dbbc4665bd6dd
lvhh <1259449128@qq.com>
2020-02-04 00:41:53 +0800

    navBar

--- components/public/header/index.vue
+++ components/public/header/index.vue
@@ -6,21 +6,26 @@
     <el-col :span="6">
         <user />
     </el-col>
-    <el-col :span="12"></el-col>
+    <el-col :span="12" class="nav">
+        <navBar />
+    </el-col>
 </el-row>
 </template>
 
 <script>
 import City from './city.vue'
 import User from './user'
+import navBar from './nav'
 export default {
     components:{
         City,
-        User
+        User,
+        navBar
     }
 }
 </script>
 
 <style lang="scss">
-    @import '../../../assets/css/public/header/header.scss'
+    @import '../../../assets/css/public/header/header.scss';
+    @import '../../../assets/css/public/layout.scss'
 </style>
---
+++ components/public/header/nav.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<ul class="navFirst">
+			<li>
+				<nuxt-link to="/myMt">我的美团</nuxt-link>
+				<ul class="navFirstDown">
+					<li><nuxt-link to="/myList">我的订单</nuxt-link></li>
+					<li><nuxt-link to="/myCollect">我的收藏</nuxt-link></li>
+					<li><nuxt-link to="/myTicket">抵用券</nuxt-link></li>
+					<li><nuxt-link to="/set">账户设置</nuxt-link></li>
+				</ul>
+			</li>
+			<li><nuxt-link to="/myPhone">我的手机</nuxt-link></li>
+		</ul>
+	</div>
+</template>
