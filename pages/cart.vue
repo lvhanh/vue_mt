f927b0b01a36f465b6d7669891c70607fb207714
lvhh <1259449128@qq.com>
2020-04-07 17:11:28 +0800

    start cart

---
+++ pages/cart.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="m-list">
+        <mlist />
+    </div>
+</template>
+
+<script>
+import Mlist from '../components/cart/list'
+export default {
+    components : {
+        Mlist
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
